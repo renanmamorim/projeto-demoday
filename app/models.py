ebd77bf54988eb3dbcee08394550188408d39218
Caroline Carvalho <carol.carvalhomaciel@hotmail.com>
2019-07-06 01:20:54 -0300

    Criar model empresa

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Empresa(models.Model):
+
+    nome = models.CharField(max_length=20)
+    cnpj = models.CharField(max_length=13)
+    email = models.EmailField()
+    telefone = models.IntegerField()
+    endereco = models.CharField(max_length=200)
+
+def __str__(self):
+        return self.nome
+
+
+
+
